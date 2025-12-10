29368d914e89df54c9d862df80035fc6a8496658
wasaimomoa <wasaimomoa@gmail.com>
2025-12-10 17:59:06 +0800

    Update CUSTOMER_SITES with new entries and names

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,11 @@
 const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+        name: '七七',
+    }
+    dbzy: {
+        api: 'https://dbzy.tv/api.php/provide/vod',
+        name: '豆瓣',
     }
 };
 
