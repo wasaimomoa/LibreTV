ba193ebb7e411ca759f74cfb95a11512f6f98762
wasaimomoa <wasaimomoa@gmail.com>
2026-02-07 08:38:26 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -125,36 +125,20 @@ const API_SITES = {
     
     ckzy: {
          api: 'https://ckzy.me/api.php/provide/vod/',
-         name: '*CK资源',
+         name: '* CK资源',
      },
      jkun: {
-         api: 'https://jkunzyapi.com',
-         name: '*jkun资源',
-     },
-     bwzy: {
-         api: 'https://api.bwzym3u8.com',
-         name: '*百万资源',
-     },
-     souav: {
-         api: 'https://api.souavzy.vip',
-         name: '*souav资源',
-     },
-     r155: {
-         api: 'https://155api.com',
-         name: '*155资源',
-     },
-     lsb: {
-         api: 'https://apilsbzy1.com',
-         name: '*lsb资源',
+         api: 'https://jkunzyapi.com/api.php/provide/vod',
+         name: '* jkun资源',
      },
      huangcang: {
-         api: 'https://hsckzy.vip',
-         name: '*黄色仓库',
+         api: 'https://hsckzy888.com/api.php/provide/vod',
+         name: '* 黄色仓库',
          detail: 'https://hsckzy.vip'
      },
      yutu: {
-         api: 'https://yutuzy10.com',
-         name: '*玉兔资源',
+         api: 'https://apiyutu.com/api.php/provide/vod',
+         name: '* 玉兔资源',
      },
 
     // 下面是资源失效率高的API源，不建议使用
